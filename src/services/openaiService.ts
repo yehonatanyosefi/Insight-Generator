a3d578211bb1ff765ac427a5d18df7d9c49f0617
eswlab-dev <roy.b@eswlab.com>
2023-06-20 17:53:34 +0300

    fixed insight data body

--- src/services/openaiService.ts
+++ src/services/openaiService.ts
@@ -1,9 +1,10 @@
 import axios from 'axios'
 
 export async function getInsightsData(boardData: any) {
+   console.log("file: openaiService.ts:4 -> getInsightsData -> boardData:", boardData)
    try {
 
-    const res = await axios.post('http://localhost:3030/openai', boardData)
+    const res = await axios.post('http://localhost:3030/openai', {boardData})
     return res.data
 
    } catch(err) {
